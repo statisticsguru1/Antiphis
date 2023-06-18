8a27ee7f2ec9f6292fd6b3dfd61830a3d8026a65
statisticsguru1 <30623588+statisticsguru1@users.noreply.github.com>
2023-06-18 16:57:40 +0000

    Adding last known changes

--- app.py
+++ app.py
@@ -2,10 +2,11 @@
 from get__features import *
 import pandas as pd
 import tensorflow as tf
-from tensorflow_addons import F1Score
+import tensorflow_addons
 import numpy as np
 import pickle
 import json
+import shiny
 from shiny import App, render, ui,reactive, ui,run_app
 from htmltools import TagList, div
 
@@ -68,30 +69,39 @@ app_ui = ui.page_fluid(
     }
         """
     ),
-    ui.h3(""),
-           ui.tags.br(),
-           ui.tags.br(),
-           ui.tags.br(),
-           ui.tags.br(),
-    ui.row(
-        ui.column(
-            3,),
-        ui.column(
-            6,
-    ui.input_text(id="url",label="",placeholder="Enter url",value="http://example.com",width='700px'),
-        ),
-        ui.column(
-            3,)),
-    ui.row(
-        ui.column(
-            4,),
-        ui.column(
-            4,
-    ui.input_action_button(id="Go",label="check",width="200px"),
-        ),
-        ui.column(
-            4,)),
-)
+    ui.navset_tab(
+       ui.nav("URL Check",
+          ui.h3(""),
+          ui.tags.br(),
+          ui.tags.br(),
+          ui.tags.br(),
+          ui.tags.br(),
+          ui.row(ui.column(3,),
+                 ui.column(6,
+                   ui.input_text(id="url",label="",placeholder="Enter url",value="http://example.com",width='700px'),),
+                 ui.column(3,)),
+          ui.row(ui.column(4,),
+                 ui.column(4,
+                           ui.input_action_button(id="Go",label="check",width="200px"),),
+                 ui.column(4,))),
+       ui.nav("Extracted Features",
+          ui.row(
+             ui.column(4,
+                        ui.output_ui("txt_title30"),
+                        ui.output_text_verbatim("lexical_feats"),),
+             ui.column(4,
+                        ui.output_ui("txt_title31"),
+                        ui.output_text_verbatim("content_feats"),),
+             ui.column(4,
+                        ui.output_ui("txt_title32"),
+                        ui.output_text_verbatim("host_feats"),),
+          ),
+          ui.row(
+             ui.column(12,
+                        ui.output_ui("txt_title33"),
+                        ui.output_text_verbatim("final_result"),),
+             )
+             )))
 
 def server(input, output, session):
     @reactive.Calc
@@ -112,8 +122,8 @@ def server(input, output, session):
         host=hostinst.run()
         print("Data preparation complete")
         return [lexical,content,host]
-      
-      
+    
+    
     @reactive.Calc
     @reactive.event(input.Go)
     def combined_features():
@@ -140,42 +150,42 @@ def server(input, output, session):
       dat=combined_features()
       ANNpred=np.argmax(ANN.predict(np.asarray(dat).astype(np.int)),axis=1)
       print("Ann prediction complete")
-      return ANNpred
+      return to_categorical(ANNpred)
     
     @reactive.Calc
     def svc_pred(): 
       dat=combined_features()
       svcpred=svc.predict(dat)
       print("SVC prediction complete")
-      return svcpred
+      return to_categorical(svcpred)
     
     @reactive.Calc
     def boosted_pred(): 
       dat=combined_features()
       boostedpred=boosted.predict(dat)
       print("boosted prediction complete")
-      return boostedpred
+      return to_categorical(boostedpred)
     
     @reactive.Calc
     def DT_pred(): 
       dat=combined_features()
       DTpred=DT.predict(dat)
       print("DT prediction complete")
-      return DTpred
+      return to_categorical(DTpred)
 
     @reactive.Calc
     def KNN_pred(): 
       dat=combined_features()
       KNNpred=KNN.predict(dat)
       print("KNN prediction complete")
-      return KNNpred
+      return to_categorical(KNNpred)
 
     @reactive.Calc
     def RF_pred(): 
       dat=combined_features()
       RFpred=RF.predict(dat)
       print("RF prediction complete")
-      return RFpred
+      return to_categorical(RFpred)
 
     @output
     @render.ui
@@ -191,6 +201,13 @@ def server(input, output, session):
         acc=round(100*accuracies['SVM'],2)
         return f"Support Vector Machine({acc}%): {pred}"
 
+    @output
+    @render.text
+    @reactive.event(input.Go)
+    def boosted_out():
+        pred=boosted_pred()
+        acc=round(100*accuracies['Boosted'],2)
+        return f"Boosted Decison tree({acc}%): {pred}"
 
     @output
     @render.text
@@ -221,14 +238,6 @@ def server(input, output, session):
     @reactive.event(input.Go)
     def txt_title2():
         return ui.tags.h3("Best model's results")
-
-    @output
-    @render.text
-    @reactive.event(input.Go)
-    def boosted_out():
-        pred=boosted_pred()
-        acc=round(100*accuracies['Boosted'],2)
-        return f"Boosted Decison tree({acc}%): {pred}"
       
     @output
     @render.text
@@ -242,23 +251,83 @@ def server(input, output, session):
     @reactive.event(input.Go)
     def combined_outs():
       return {'Support vector machine':svc_pred(),
-    'Decision Tree':DT_pred(),
-    'Boosted Decision trees':boosted_pred(),
-    'Random Forest': RF_pred(),
-    'K nearest Neigbour':KNN_pred(),
-    'Artificial Neural Network':ANN_pred()}
+              'Decision Tree':DT_pred(),
+              'Boosted Decision trees':boosted_pred(),
+              'Random Forest': RF_pred(),
+              'K nearest Neigbour':KNN_pred(),
+              'Artificial Neural Network':Ann_pred()}
+
+    @output
+    @render.ui
+    @reactive.event(combined_outs)
+    def txt_title30():
+        return ui.tags.h3("Lexical features")
+
+    @output
+    @render.ui
+    @reactive.event(combined_outs)
+    def txt_title31():
+        return ui.tags.h3("Content features")
+
+    @output
+    @render.ui
+    @reactive.event(combined_outs)
+    def txt_title32():
+        return ui.tags.h3("Host features") 
+
+    @output
+    @render.ui
+    @reactive.event(combined_outs)
+    def txt_title33():
+        return ui.tags.h3("Predicted status")  
+
+    @output
+    @render.text
+    @reactive.event(combined_outs)
+    def lexical_feats():
+        features=extract_features()
+        lexical=features[0]
+        formatted_pairs = [f"{key:<15}: {value}" for key, value in lexical.items()]
+        return "\n".join(formatted_pairs)
+    
+    @output
+    @render.text
+    @reactive.event(combined_outs)
+    def content_feats():
+        features=extract_features()
+        content=features[1]
+        formatted_pairs = [f"{key:<15}: {value}" for key, value in content.items()]
+        return "\n".join(formatted_pairs)
+
+    @output
+    @render.text
+    @reactive.event(combined_outs)
+    def host_feats():
+        features=extract_features()
+        host=features[2]
+        formatted_pairs = [f"{key:<15}: {value}" for key, value in host.items()]
+        return "\n".join(formatted_pairs)
+
+    @output
+    @render.text
+    @reactive.event(combined_outs)
+    def final_result():
+        res=combined_outs()
+        formatted_pairs = [f"{key}: {value}" for key, value in res.items()]
+        rows = [formatted_pairs[i:i+5] for i in range(0, len(formatted_pairs), 5)]
+        return "\n".join("    ".join(row) for row in rows)
 
 
     @reactive.Effect
-    @reactive.event(combined_outs())
+    @reactive.event(combined_outs)
     def _():
       m=ui.modal(
         ui.output_ui("txt_title1"),
         ui.output_text("svc_out"),
         ui.output_text("DT_out"),
+        ui.output_text("ANN_out"),
         ui.output_text("RF_out"),
         ui.output_text("KNN_out"),
-        ui.output_text("ANN_out"),
         ui.output_ui("txt_title2"),
         ui.output_text("boosted_out"),
         easy_close=True,
@@ -266,13 +335,10 @@ def server(input, output, session):
           ui.modal_button("Cancel"),
           ui.input_action_button("ok", "OK")))
       ui.modal_show(m)
-        
-      
-
+    
     @reactive.Effect
     @reactive.event(input.ok)
     def removemod():
-      
-      ui.modal_remove()
-      
+       ui.modal_remove()
+
 app = App(app_ui, server)
